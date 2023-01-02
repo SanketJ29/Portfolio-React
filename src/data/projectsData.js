dc22032c9d129040b783cc498acf0c4e86d21f10
Sanket Anand Jaiswal <103429178+SanketJ29@users.noreply.github.com>
2023-01-02 22:06:33 +0530

    Update projectsData.js

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -10,17 +10,17 @@ export const projectsData = [
     {
         id: 1,
         projectName: 'Pinterest Clone',
-        projectDesc: 'It is a clone of Pinterest which uses Unsplash API for generating high-quality royalty free images. It is responsive.',
-        tags: ['React', 'SCSS', 'Unsplash API'],
+        projectDesc: 'This website is a clone of Pinterest which uses Unsplash Image API for generating high-quality royalty free images. It has been designed to be responsive, ensuring a seamless experience across devices.',
+        tags: ['ReactJS', 'SCSS', 'Unsplash API'],
         code: 'https://github.com/SanketJ29/Pinterest-Clone',
         demo: 'https://sanket-pinterest.netlify.app/',
         image: six
     },
     {
         id: 2,
-        projectName: 'React Portfolio',
-        projectDesc: 'It is a responsive Portfolio website built using React. Lottie-react is used to use lightweight animations. EmailJS is used for contact form',
-        tags: ['React', 'Material Ui', 'Lottie-react', 'EmailJS'],
+        projectName: 'React-Portfolio',
+        projectDesc: 'This website is a responsive Portfolio website built using ReactJS. Lottie-react is used to use lightweight animations. EmailJS is used for contact form',
+        tags: ['ReactJS', 'Material Ui', 'Lottie-react', 'EmailJS'],
         code: 'https://github.com/SanketJ29/Portfolio-React',
         demo: 'https://sanketjaiswal.netlify.app/',
         image: seven
@@ -28,8 +28,8 @@ export const projectsData = [
     {
         id: 3,
         projectName: 'Spotify Web Clone',
-        projectDesc: 'It is a clone of Spotify web. It allows user to listen to the songs and can download it.',
-        tags: ['React', 'React-Redux'],
+        projectDesc: 'This website is a clone of Spotify web. It allows user to listen to the songs and can download it.',
+        tags: ['ReactJS', 'React-Redux'],
         code: 'https://github.com/SanketJ29/Spotify_MusicPlayer',
         demo: 'https://sanket-spotifyweb.netlify.app/',
         image: eight
@@ -37,7 +37,7 @@ export const projectsData = [
     {
         id: 4,
         projectName: 'RainStorm - A Weather App',
-        projectDesc: 'RainStorm ia a weather app which uses OpenWeatherMap API to display weather along with humidity and temperature. It has theme-toggle option.',
+        projectDesc: 'RainStorm is a weather app which uses OpenWeatherMap API to display weather along with humidity and temperature. It has theme-toggle option.',
         tags: ['JavaScript', 'HTML', 'CSS'],
         code: 'https://github.com/SanketJ29/RainStorm',
         demo: 'https://rainstorm.netlify.app/',
@@ -46,7 +46,7 @@ export const projectsData = [
     {
         id: 5,
         projectName: 'JS calculator',
-        projectDesc: 'A simple JavaScript calculator with theme-toggle option.',
+        projectDesc: 'This is a simple JavaScript calculator with theme-toggle option.',
         tags: ['JavaScript', 'HTML', 'CSS'],
         code: 'https://github.com/SanketJ29/JS_Calculator',
         demo: 'https://calculator-sanket.netlify.app/',
@@ -55,7 +55,7 @@ export const projectsData = [
     {
         id: 6,
         projectName: 'Old Portfolio',
-        projectDesc: 'A simple responsive portfolio website built using pure JavaScript.',
+        projectDesc: 'This is a simple responsive portfolio website built using HTML, CSS and JavaScript.',
         tags: ['JavaScript', 'HTML', 'CSS'],
         code: 'https://github.com/SanketJ29/Portfolio_Old',
         demo: 'https://portfolio-sanket.netlify.app/',
