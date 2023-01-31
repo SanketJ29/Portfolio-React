b53766279924cd1bd2d66fe37d471416630e6e73
Sanket Anand Jaiswal <103429178+SanketJ29@users.noreply.github.com>
2023-01-31 14:08:31 +0530

    Update aboutData.js

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,6 +1,6 @@
 export const aboutData = {
     title: "Who I am",
     description1: "My name is Sanket Jaiswal. I'm a Web developer and a Computer Engineering student based in Mumbai, India.",
-    description2: "As a motivated and skilled programmer, I have a strong foundation in object-oriented programming and web development. I am proficient in Java, Python, and JavaScript, and I have excellent communication skills and a team-oriented approach. I am also adept at quickly learning and effectively implementing new technical concepts.",
+    description2: "As a motivated and skilled programmer, I have a strong foundation in object-oriented programming and web development. I am proficient in Java, Python,JavaScript along with excellent communication skills and a team-oriented approach. I am also adept at quickly learning and effectively implementing new technical concepts.",
     image: 1
 }
