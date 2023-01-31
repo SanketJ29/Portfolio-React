40992d5c42b667988f3e0f833afdff96b9e55274
Sanket Anand Jaiswal <103429178+SanketJ29@users.noreply.github.com>
2023-01-31 14:07:40 +0530

    Add files via upload

---
+++ src/data/certificate-data.js
@@ -0,0 +1,67 @@
+import certificate1 from "../assets/certificate/javabasic.png";
+import certificate2 from "../assets/certificate/1669827410964_page-0001.jpg";
+import certificate3 from "../assets/certificate/pythonbasic.png";
+import certificate4 from "../assets/certificate/3760839_1662820288_page-0001.jpg";
+import certificate5 from "../assets/certificate/problemsolving.png";
+import certificate6 from "../assets/certificate/greatlearning.png";
+import certificate7 from "../assets/certificate/reactjs.jpg";
+import certificate8 from "../assets/certificate/Hack-AI-Thon.jpg";
+import certificate9 from "../assets/certificate/CSI_CodeKombat.jpg";
+
+export const certificateData = [
+  {
+    id: "02",
+    title: "BITS Pilani Postman API Hackathon",
+    image: certificate2,
+    link: "https://media.licdn.com/dms/document/C4D1FAQE6sq--BuN3Ug/feedshare-document-pdf-analyzed/0/1669827410964?e=1673481600&v=beta&t=EFHJELTWkvM7NFnRtv14ILye8VHTVMSBNeqt9t-2PBU" ,
+  },
+
+  {
+    id: "05",
+    title: "HackerRank Problem Solving Skill Certification",
+    image: certificate5,
+    link: "https://www.hackerrank.com/certificates/8497e16dce50",
+  },
+  {
+    id: "06",
+    title: "Great Learing-Java Programming",
+    image: certificate6,
+    link: "https://olympus.mygreatlearning.com/courses/12385/certificate",
+  },
+  {
+    id: "01",
+    title: "HackerRank-Java Basic Skill Certification",
+    image: certificate1,
+    link: "https://www.hackerrank.com/certificates/6056f1c381da",
+  },
+  {
+    id: "03",
+    title: "HackerRank Python Basic Skill Certification",
+    image: certificate3,
+    link: "https://www.hackerrank.com/certificates/adf02dd92c67",
+  },
+  {
+    id: "04",
+    title: "Simplilearn-Programming with Python",
+    image: certificate4,
+    link: "https://certificates.simplicdn.net/share/3760839_1662820288.pdf",
+  },
+  {
+    id: "07",
+    title: "Simplilearn-ReactJS for Beginners",
+    image: certificate7,
+    link: "https://certificates.simplicdn.net/share/4069161_1673008041.pdf",
+  },
+  {
+    id: "08",
+    title: "Hack-AI-Thon",
+    image: certificate8,
+    link: "https://drive.google.com/file/d/1EGx8xI2fIHARon6kVGIsk2vtfqCQK57y/view?usp=sharing",
+  },
+  {
+    id: "09",
+    title: "CSI-Code Kombat",
+    image: certificate9,
+    link: "https://drive.google.com/file/d/1f5cw03-B3zG6g6ZuOs0ue-_H8KhjGEKE/view?usp=sharing",
+  },
+];
