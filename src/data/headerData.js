d12dbe5c290f80b12a6aff68948d59c0dea9d9cf
Sanket Jaiswal <103429178+SanketJ29@users.noreply.github.com>
2023-11-04 19:20:36 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,8 +1,10 @@
+import profilep from "../assets/png/profile_sanket_3.jpg";
 
 export const headerData = {
     name: 'Sanket Jaiswal',
     title: "Hello! I am",
     desciption: "Hello! My name is Sanket Jaiswal and I am a dedicated programmer with a strong passion for learning and exploring new technologies. I am also highly motivated and enjoy solving problems.",
-    image: 'https://www.linkpicture.com/q/Picsart_22-09-18_21-25-05-453_2.jpg',
+    // image: 'https://www.linkpicture.com/q/Picsart_22-09-18_21-25-05-453_2.jpg',
+    image: profilep,
     resumePdf: 'https://drive.google.com/file/d/1nuSKd4NbizzdLLutIWFpJldZ1IIgARAv/view?usp=sharing'
 }
